6da5e3e4e661849502e8303283ba691d8d89bbd5
Justin Prim <justinprim140969@gmail.com>
2023-03-12 12:10:54 +0200

    Link to database and consumption of public api

--- app.py
+++ app.py
@@ -1,35 +1,53 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
+
+import requests
+import sqlite3
 import pandas as pd
 import numpy as np
 
+#creating an instance of flask
 app = Flask(__name__)
 
 users = [
-    {"id": 1, "name": "Thailand", "city": "Bangkok", "area": 513120},
-    {"id": 2, "name": "Australia", "city": "Canberra", "area": 7617930},
-    {"id": 3, "name": "Egypt", "city": "Cairo", "area": 1010408},
+    {"species": "Human", "name": "Justin", "status": "Alive", "rating": 10},
+    {"species": "Human", "name": "Jayden", "status": "Alive", "rating": 10},
+
 ]
 
 @app.route('/users/', methods=['GET', 'POST'])
 def welcome():
     if request.method == 'POST':
       user = request.args
+      conn = sqlite3.connect("people.db")
+      response =   requests.get("https://rickandmortyapi.com/api/character/"+user['id'])
+
+      cname = response.json()["name"]
+      cspecies =response.json()["species"]
+      cstatus = response.json()["status"]
 
       new_data = jsonify({
             'id': user['id'],
-            'name': user['name'],
-            'city': user['city'],
-            'area': [25406]
+            'species': cspecies,
+            'name': cname,
+            'status': cstatus,
+            'rating': user['rating']
         })
+      
+      cursor = conn.cursor()
+      cursor.execute("insert into person (id, name, species, status, rating) values (?, ?, ? ,? ,? )",
+            (user['id'],cname,cspecies,cstatus,user['rating']))
 
+      conn.commit()
       return (new_data) , 201
+    
     else:
-      return jsonify(users), 200
+      conn = sqlite3.connect("people.db")
+      cur = conn.cursor()
+      cur.execute("SELECT * FROM person")
+      people = cur.fetchall()
 
-@app.route('/person/')
-def hello():
-    return jsonify({'name':'Jimit',
-                    'address':'India'})
+      return jsonify(people), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)
--- new_API.py
+++ new_API.py
@@ -1,10 +1,55 @@
 import requests
 import sqlite3
 
+
+conn = sqlite3.connect("people.db")
+columns = [
+     "id INTEGER PRIMARY KEY",
+     "name VARCHAR ",
+     "species VARCHAR",
+     "status VARCHAR",
+     "rating INTEGER"
+ ]
+create_table_cmd = f"CREATE TABLE person ({','.join(columns)})"
+conn.execute(create_table_cmd)
+
+people = [
+    "1, 'Justin', 'Human', 'Alive',10",
+    "2, 'Jayden', 'Human', 'Alive',6",
+    "3, 'Love', 'Human', 'Alive',12",
+ ]
+for person_data in people:
+     insert_cmd = f"INSERT INTO person VALUES ({person_data})"
+     conn.execute(insert_cmd)
+
+conn.commit()
+
+
+cur = conn.cursor()
+cur.execute("SELECT * FROM person")
+
+
+people = cur.fetchall()
+for person in people:
+     print(person)
+
 response =   requests.get("https://rickandmortyapi.com/api/character/135")
 
-print(response.json()["name"])
-print(response.json()["species"])
-print(response.json()["status"])
+cname = response.json()["name"]
+cspecies =response.json()["species"]
+cstatus = response.json()["status"]
 print(response.status_code)
 
+cursor = conn.cursor()
+
+cursor.execute("insert into person (id, name, species, status, rating) values (?, ?, ? ,? ,? )",
+            (4,cname,cspecies,cstatus,5))
+
+conn.commit()
+
+cur = conn.cursor()
+cur.execute("SELECT * FROM person")
+
+people = cur.fetchall()
+for person in people:
+     print(person)
